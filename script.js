3a9829cd23b1da270bd218b60df28f9f93869623
chuadrian <chuadrian243@gmail.com>
2024-03-01 19:49:55 +0100

    Add files via upload
    changes to responsiveness

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function headerShadow() {
 }
 
 var typingEffect = new Typed(".typedText",{
-    strings : ["Designer", "Developer", "Content Editor"],
+    strings : ["Designer", "Developer"],
     loop : true,
     typeSpeed : 100,
     backSpeed : 80,
